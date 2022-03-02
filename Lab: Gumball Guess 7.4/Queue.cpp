55393c2992fae0d3237a59a17148d54fc059cdc5
Coleton Watt <electricninja102@gmail.com>
2022-03-02 12:49:13 -0700

    Initial Commit

---
+++ Lab: Gumball Guess 7.4/Queue.cpp
@@ -0,0 +1,8 @@
+//
+//  Queue.cpp
+//  Lab: Gumball Guess 7.4
+//
+//  Created by Coleton Watt on 3/2/22.
+//
+
+#include "Queue.hpp"
---
+++ Lab: Gumball Guess 7.4/Queue.hpp
@@ -0,0 +1,13 @@
+//
+//  Queue.hpp
+//  Lab: Gumball Guess 7.4
+//
+//  Created by Coleton Watt on 3/2/22.
+//
+
+#ifndef Queue_hpp
+#define Queue_hpp
+
+#include <stdio.h>
+
+#endif /* Queue_hpp */
